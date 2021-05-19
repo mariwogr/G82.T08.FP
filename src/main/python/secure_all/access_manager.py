4d5158033b3aa6d19bb255461d2a0d7daeb3869e
mariwogr <100428982@alumnos.uc3m.es>
2021-05-19 13:37:19 +0200

    3a funcionalidad hecha hasta la mitad + o -

--- src/main/python/secure_all/access_manager.py
+++ src/main/python/secure_all/access_manager.py
@@ -1,13 +1,16 @@
 """Module AccessManager with AccessManager Class """
 
+import json
 from secure_all.data.access_key import AccessKey
 from secure_all.data.access_request import AccessRequest
 from secure_all.data.open_door import OpenDoor
+from secure_all import AccessManagementException, KeysJsonStore
 
 
 class AccessManager:
     """AccessManager class, manages the access to a building implementing singleton """
     #pylint: disable=too-many-arguments,no-self-use,invalid-name, too-few-public-methods
+    #pylint: disable=inconsistent-return-statements
     class __AccessManager:
         """Class for providing the methods for managing the access to a building"""
 
@@ -28,6 +31,42 @@ class AccessManager:
             if AccessKey.create_key_from_id(key).is_valid():
                 OpenDoor(key).store_access()
                 return True
+
+        def revoke_key(self, file):
+            """Revokes the key"""
+            try:
+                with open(file, "r", encoding="utf-8", newline="") as json_file:
+                    data = json.load(json_file)
+            except FileNotFoundError as ex:
+                raise AccessManagementException\
+                    ("El archivo de entrada tiene algún problema \
+                     relacionado con su formato o con su acceso.") \
+                    from ex
+            except json.JSONDecodeError as ex:
+                raise AccessManagementException("JSON Decode Error - Wrong JSON Format") from ex
+
+            key = data["Key"]
+            revocation = data["Revocation"]
+
+            try:
+                AccessKey.create_key_from_id(key).is_valid()
+            except AccessManagementException as ex:
+                raise AccessManagementException(" La clave recibida ha caducado.") from ex
+
+            store = KeysJsonStore().find_item(key)
+
+            if store is None:
+                raise AccessManagementException("La clave recibida no existe")
+
+            emails = store["_AccessKey__notification_emails"]
+
+            if store["_AccessKey__revocation"]:
+                raise AccessManagementException("La clave fue revocada previamente por este método")
+
+
+
+
+
 
     __instance = None
 
